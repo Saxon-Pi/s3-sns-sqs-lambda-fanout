b2a1451adbd93ee75c8547d94f9376c422a01782
Saxon-Pi <ts.8849.p@gmail.com>
2025-11-18 20:17:22 +0900

    create rotate lambda

---
+++ lambdas/rotate/sandbox/rotate.ts
@@ -0,0 +1,20 @@
+import jimp from 'jimp'
+import * as path from 'node:path'
+
+const REPOSITORY_TOP = path.resolve(__dirname, '../../../')
+
+async function main(){
+  // 画像の読み込み
+  const imagePath = path.join(REPOSITORY_TOP, "images/daru-nyanko.jpg")
+  console.log(`reading an image from: ${imagePath}`)
+
+  const image = await jimp.read(imagePath)
+
+  // 変換処理
+  image.rotate(90)
+
+  // 画像の保存
+  image.write('rotate.png')
+}
+
+main()
